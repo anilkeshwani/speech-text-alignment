46724f10663956856831ca1569c5b74eef4f3a34
anilkeshwani <anilkeshwani@hotmail.com>
2024-04-24 08:41:10 +0000

    Bash best practices inc. subshell in test_fairseq_align_and_segment - works ✅; add tests/outputs to .gitignore

--- tests/test_fairseq_align_and_segment.sh
+++ tests/test_fairseq_align_and_segment.sh
@@ -2,13 +2,17 @@
 
 set -euo pipefail
 
-PROJECT_ROOT_DIR="/Users/anilkeshwani/Desktop/speech-text-alignment"
+PROJECT_ROOT_DIR="${SPEECH_TEXT_ALIGN_ROOT_DIR:-${HOME}/speech-text-alignment}"
+SARDALIGN_DIR="${PROJECT_ROOT_DIR}/sardalign"
+AUDIO_PATH="${PROJECT_ROOT_DIR}/tests/data/LJ001-0001_SR_16000.wav"
+OUTPUT_DIR="${PROJECT_ROOT_DIR}/tests/output/$(basename "${AUDIO_PATH%.*}")"
 
-cd "${PROJECT_ROOT_DIR}/sardalign"
-
-python align_and_segment.py \
-    --audio "${PROJECT_ROOT_DIR}/tests/data/LJ001-0001.wav" \
-    --text_filepath "${PROJECT_ROOT_DIR}/tests/data/LJ001-0001.txt" \
-    --lang 'eng' \
-    --outdir "../tests_output" \
-    --uroman "${PROJECT_ROOT_DIR}/submodules/uroman/bin"
+(
+    cd "$SARDALIGN_DIR"
+    python align_and_segment.py \
+        --audio "$AUDIO_PATH" \
+        --text_filepath "${PROJECT_ROOT_DIR}/tests/data/LJ001-0001.txt" \
+        --lang 'eng' \
+        --outdir "$OUTPUT_DIR" \
+        --uroman "${PROJECT_ROOT_DIR}/submodules/uroman/bin"
+)
