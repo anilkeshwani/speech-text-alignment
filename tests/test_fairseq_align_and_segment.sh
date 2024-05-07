76d20518d892a2b8df889028a7a25b7887d6831c
anilkeshwani <anilkeshwani@hotmail.com>
2024-05-07 15:28:37 +0000

    Clean up tests/test_fairseq_align_and_segment.sh

--- tests/test_fairseq_align_and_segment.sh
+++ tests/test_fairseq_align_and_segment.sh
@@ -5,14 +5,17 @@ set -euo pipefail
 PROJECT_ROOT_DIR="${SPEECH_TEXT_ALIGN_ROOT_DIR:-${HOME}/speech-text-alignment}"
 SARDALIGN_DIR="${PROJECT_ROOT_DIR}/sardalign"
 AUDIO_PATH="${PROJECT_ROOT_DIR}/tests/data/LJ001-0001_SR_16000.wav"
+TEXT_FILEPATH="${PROJECT_ROOT_DIR}/tests/data/LJ001-0001.txt"
+LANG='eng'
 OUTPUT_DIR="${PROJECT_ROOT_DIR}/tests/output/$(basename "${AUDIO_PATH%.*}")"
+UROMAN_BINARY="${PROJECT_ROOT_DIR}/submodules/uroman/bin"
 
 (
     cd "$SARDALIGN_DIR"
     python align_and_segment.py \
         --audio "$AUDIO_PATH" \
-        --text_filepath "${PROJECT_ROOT_DIR}/tests/data/LJ001-0001.txt" \
-        --lang 'eng' \
+        --text_filepath "$TEXT_FILEPATH" \
+        --lang "$LANG" \
         --outdir "$OUTPUT_DIR" \
-        --uroman "${PROJECT_ROOT_DIR}/submodules/uroman/bin"
+        --uroman "$UROMAN_BINARY"
 )
