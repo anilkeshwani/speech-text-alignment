21ce6b33cab13686c931f188b1ed4c5c9c933517
anilkeshwani <anilkeshwani@hotmail.com>
2024-05-08 17:10:03 +0000

    Format and isort Kshitij's snippets

--- scripts/kshitij_hubert_mls_dirty/txt2tsv.py
+++ scripts/kshitij_hubert_mls_dirty/txt2tsv.py
@@ -1,8 +1,9 @@
 import os
 
+
 root = "/mnt/scratch/kshitij/SPGIspeech/shards/"
 
 files = os.listdir(root)
 
 for i in files:
-    os.rename(root+i,root+i[0:-3]+'tsv')
+    os.rename(root + i, root + i[0:-3] + "tsv")
