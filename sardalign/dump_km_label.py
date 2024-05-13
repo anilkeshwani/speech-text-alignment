30bc919c6a9c033b5cd457132c237fa55a44d82f
anilkeshwani <anilkeshwani@hotmail.com>
2024-05-13 13:01:21 +0000

    Format dump_km_label.py

--- sardalign/dump_km_label.py
+++ sardalign/dump_km_label.py
@@ -7,11 +7,11 @@ import logging
 import os
 import sys
 
-import numpy as np
-
 import joblib
+import numpy as np
 import torch
 import tqdm
+
 
 logging.basicConfig(
     format="%(asctime)s | %(levelname)s | %(name)s | %(message)s",
@@ -26,7 +26,7 @@ class ApplyKmeans(object):
     def __init__(self, km_path):
         self.km_model = joblib.load(km_path)
         self.C_np = self.km_model.cluster_centers_.transpose()
-        self.Cnorm_np = (self.C_np ** 2).sum(0, keepdims=True)
+        self.Cnorm_np = (self.C_np**2).sum(0, keepdims=True)
 
         self.C = torch.from_numpy(self.C_np)
         self.Cnorm = torch.from_numpy(self.Cnorm_np)
@@ -36,18 +36,10 @@ class ApplyKmeans(object):
 
     def __call__(self, x):
         if isinstance(x, torch.Tensor):
-            dist = (
-                x.pow(2).sum(1, keepdim=True)
-                - 2 * torch.matmul(x, self.C)
-                + self.Cnorm
-            )
+            dist = x.pow(2).sum(1, keepdim=True) - 2 * torch.matmul(x, self.C) + self.Cnorm
             return dist.argmin(dim=1).cpu().numpy()
         else:
-            dist = (
-                (x ** 2).sum(1, keepdims=True)
-                - 2 * np.matmul(x, self.C_np)
-                + self.Cnorm_np
-            )
+            dist = (x**2).sum(1, keepdims=True) - 2 * np.matmul(x, self.C_np) + self.Cnorm_np
             return np.argmin(dist, axis=1)
 
 
@@ -62,7 +54,7 @@ def get_feat_iterator(feat_dir, split, nshard, rank):
         feat = np.load(feat_path, mmap_mode="r")
         assert feat.shape[0] == (offsets[-1] + lengs[-1])
         for offset, leng in zip(offsets, lengs):
-            yield feat[offset: offset + leng]
+            yield feat[offset : offset + leng]
 
     return iterate, len(lengs)
 
