106c83e4ee54ded1485fed492186e4fd4a4135e0
anilkeshwani <anilkeshwani@hotmail.com>
2024-05-24 16:55:33 +0000

    Tidy up sardalign/text_normalization.py

--- sardalign/text_normalization.py
+++ sardalign/text_normalization.py
@@ -2,85 +2,66 @@ import re
 import unicodedata
 
 from sardalign.norm_config import norm_config
+from unidecode import unidecode
 
 
 def text_normalize(
-    text: str, iso_code: str, lower_case: bool = True, remove_numbers: bool = True, remove_brackets: bool = False
-):
-    """Given a text, normalize it by changing to lower case, removing punctuations, removing words that only contain digits and removing extra spaces
+    text: str,
+    iso_code: str,
+    lower_case: bool = True,
+    remove_numbers: bool = True,
+    remove_brackets: bool = False,
+) -> str:
+    """Normalize text by lowercasing, removing punctuation and words containing only digits, and collapsing spaces.
 
     Args:
-        text : The string to be normalized
-        iso_code :
-        remove_numbers : Boolean flag to specify if words containing only digits should be removed
+        text (str): String to be normalized
+        iso_code (str): Language ISO code
+        lower_case (bool, optional): Lowercase. Defaults to True.
+        remove_numbers (bool, optional): Remove numbers. Defaults to True.
+        remove_brackets (bool, optional): Remove brackets. Defaults to False.
 
     Returns:
-        normalized_text : the string after all normalization
-
+        str: Normalized string
     """
-
     config = norm_config.get(iso_code, norm_config["*"])
-
     for field in ["lower_case", "punc_set", "del_set", "mapping", "digit_set", "unicode_norm"]:
         if field not in config:
             config[field] = norm_config["*"][field]
-
     text = unicodedata.normalize(config["unicode_norm"], text)
-
-    # Convert to lower case
-
     if config["lower_case"] and lower_case:
         text = text.lower()
-
-    # brackets
-
-    # always text inside brackets with numbers in them. Usually corresponds to "(Sam 23:17)"
+    # always text inside brackets with numbers in them. usually corresponds to "(sam 23:17)"
     text = re.sub(r"\([^\)]*\d[^\)]*\)", " ", text)
     if remove_brackets:
         text = re.sub(r"\([^\)]*\)", " ", text)
-
-    # Apply mappings
-
+    # apply mappings
     for old, new in config["mapping"].items():
         text = re.sub(old, new, text)
-
-    # Replace punctutations with space
-
+    # replace punctutations with space
     punct_pattern = r"[" + config["punc_set"]
-
     punct_pattern += "]"
-
     normalized_text = re.sub(punct_pattern, " ", text)
-
     # remove characters in delete list
-
     delete_patten = r"[" + config["del_set"] + "]"
-
     normalized_text = re.sub(delete_patten, "", normalized_text)
-
     # Remove words containing only digits
-    # We check for 3 cases  a)text starts with a number b) a number is present somewhere in the middle of the text c) the text ends with a number
-    # For each case we use lookaround regex pattern to see if the digit pattern in preceded and followed by whitespaces, only then we replace the numbers with space
+    # We check for 3 cases:
+    #   a) text starts with a number
+    #   b) a number is present somewhere in the middle of the text
+    #   c) the text ends with a number
+    # For each case we use lookaround regex pattern to see if the digit pattern is preceded and
+    # followed by whitespaces, only then we replace the numbers with space.
     # The lookaround enables overlapping pattern matches to be replaced
-
     if remove_numbers:
-
         digits_pattern = "[" + config["digit_set"]
-
         digits_pattern += "]+"
-
         complete_digit_pattern = (
             r"^" + digits_pattern + "(?=\s)|(?<=\s)" + digits_pattern + "(?=\s)|(?<=\s)" + digits_pattern + "$"
         )
-
         normalized_text = re.sub(complete_digit_pattern, " ", normalized_text)
-
     if config["rm_diacritics"]:
-        from unidecode import unidecode
-
         normalized_text = unidecode(normalized_text)
-
-    # Remove extra spaces
+    # remove extra spaces
     normalized_text = re.sub(r"\s+", " ", normalized_text).strip()
-
     return normalized_text
