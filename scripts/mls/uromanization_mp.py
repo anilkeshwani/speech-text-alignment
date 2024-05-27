fcc3a95a474b3a6b05b9be44326745d9d0331eeb
anilkeshwani <anilkeshwani@hotmail.com>
2024-05-27 16:54:45 +0000

    Add parallelised normalization to scripts/mls/uromanization_mp.py

--- scripts/mls/uromanization_mp.py
+++ scripts/mls/uromanization_mp.py
@@ -29,6 +29,7 @@ def parse_args() -> Namespace:
     parser.add_argument("input_jsonl", type=Path, help="Path to input JSON lines file")
     parser.add_argument("--output-jsonl", type=Path, default=None, help="Output JSON lines path")
     parser.add_argument("--lang", type=str, default="eng", help="ISO code of the language")
+    parser.add_argument("--n-processes", type=int, default=None, help="Number of parallel processes")
     parser.add_argument("--text-key", type=str, default="transcript", help="Key of text field in JSON lines manifest")
     parser.add_argument("--uroman-key", type=str, default="uroman", help="Key for uroman tokens in JSON lines manifest")
     parser.add_argument(
@@ -40,16 +41,18 @@ def parse_args() -> Namespace:
     args = parser.parse_args()
     if args.output_jsonl is None:
         args.output_jsonl = args.input_jsonl.with_stem(args.input_jsonl.stem + "_uroman")
+    if args.n_processes is None:
+        args.n_processes = max(1, mp.cpu_count() - 1)
     return args
 
 
 UROMAN_DATA_DIR = PROJECT_ROOT / "submodules" / "uroman" / "data"
 
 
-def process_chunk(chunk: list[list[str]], lang: str, uroman=Uroman(UROMAN_DATA_DIR)):
-    chunk_tokens_s = []
+def uromanize_chunk(chunk: list[list[str]], lang: str, uroman=Uroman(UROMAN_DATA_DIR)):
+    chunk_results = []
     for nt in chunk:
-        chunk_tokens_s.append(
+        chunk_results.append(
             post_process_uroman(
                 [
                     uroman.romanize_string_core(token, lcode=lang, rom_format=RomFormat.STR, cache_p=False)
@@ -58,7 +61,14 @@ def process_chunk(chunk: list[list[str]], lang: str, uroman=Uroman(UROMAN_DATA_D
                 normalize_uroman_post=True,
             )
         )
-    return chunk_tokens_s
+    return chunk_results
+
+
+def normalize_chunk(chunk: list[list[str]], lang: str):
+    chunk_results: list[list[str]] = []
+    for transcripts in chunk:
+        chunk_results.append([text_normalize(token, lang) for token in transcripts])
+    return chunk_results
 
 
 def main(args):
@@ -67,27 +77,33 @@ def main(args):
         LOGGER.info(f"Created directory for output at {args.output_jsonl.parent}")
     dataset = read_jsonl(args.input_jsonl)
     LOGGER.info(f"Read {len(dataset)} lines from {args.input_jsonl}")
+
     transcripts_s: list[list[str]] = []
     for s in tqdm(dataset, desc="Tokenizing dataset"):
         transcripts_s.append(s[args.text_key].strip().split(args.token_delimiter))
-    norm_transcripts_s: list[list[str]] = []
-    for transcripts in tqdm(transcripts_s, desc="Normalizing transcripts"):
-        norm_transcripts_s.append([text_normalize(token, args.lang) for token in transcripts])
 
-    n_processes = mp.cpu_count() - 1
-    chunk_size = len(norm_transcripts_s) // n_processes
-    chunks = [norm_transcripts_s[i : i + chunk_size] for i in range(0, len(norm_transcripts_s), chunk_size)]
+    chunk_size = len(transcripts_s) // args.n_processes
 
-    with mp.Pool(processes=n_processes) as pool:
+    transcripts_s_chunks = [transcripts_s[i : i + chunk_size] for i in range(0, len(transcripts_s), chunk_size)]
+    with mp.Pool(processes=args.n_processes) as pool:
         results = list(
             tqdm(
-                pool.starmap(process_chunk, [(chunk, args.lang) for chunk in chunks]),
-                total=n_processes,
+                pool.starmap(normalize_chunk, [(chunk, args.lang) for chunk in transcripts_s_chunks]),
+                total=args.n_processes,
+                desc="Normalizing transcripts",
+            )
+        )
+
+    with mp.Pool(processes=args.n_processes) as pool:
+        results = list(
+            tqdm(
+                pool.starmap(uromanize_chunk, [(chunk, args.lang) for chunk in results]),
+                total=args.n_processes,
                 desc="Processing uroman",
             )
         )
 
-    tokens_s = [item for sublist in results for item in sublist]  # flatten parallel processed lists
+    tokens_s = [item for sublist in results for item in sublist]
 
     dataset = [s | {args.uroman_key: tokens} for s, tokens in zip(dataset, tokens_s)]
 
