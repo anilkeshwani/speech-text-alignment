1906f1df0350e95a9e6013e7a411bb71c4e88e2f
anilkeshwani <anilkeshwani@hotmail.com>
2024-06-11 00:27:00 +0000

    Add config variable for EXP_SPAN_LENGTH: float = 39.43 / 4 - numerator based on MLS average token length of sequence in strat. sample

--- sardalign/config.py
+++ sardalign/config.py
@@ -1,3 +1,6 @@
 LOG_LEVEL: str = "INFO"
 LOG_FORMAT: str = "%(asctime)s | %(levelname)s | %(name)s | %(message)s"
 LOG_DATEFMT: str = "%Y-%m-%d %H:%M:%S"
+
+# Dataset Processing
+EXP_SPAN_LENGTH: float = 39.43 / 4  # numerator: mean sequence length (tokens) in the MLS 25% stratified sample
