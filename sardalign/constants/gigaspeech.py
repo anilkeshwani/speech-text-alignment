fe74e1248b058f66be753f90864cdf98df7ff42c
anilkeshwani <anilkeshwani@hotmail.com>
2024-08-16 11:29:40 +0000

    Update GigaSpeech constants & requirements.txt with datasets and loosen tqdm to >=4.65.0

--- sardalign/constants/gigaspeech.py
+++ sardalign/constants/gigaspeech.py
@@ -1,8 +1,10 @@
+GS_HF_DATASET_REPO: str = "speechcolab/gigaspeech"
+
 # We keep 4 punctuations in the normalized text (see the text_tn entry in GigaSpeech.json)
-PUNCTUATION_TAGS: list[str] = ["<COMMA>", "<PERIOD>", "<QUESTIONMARK>", "<EXCLAMATIONPOINT>"]
+GS_PUNCTUATION_TAGS: list[str] = ["<COMMA>", "<PERIOD>", "<QUESTIONMARK>", "<EXCLAMATIONPOINT>"]
 
 # The Dev/Test evaluation sets are annotated by human annotators. They are instructed to label the
 # entire audio file without "gaps". So for non-speech segments, garbage utterance tags are used
 # instead. We recommend our users to discard these utterances in your training.
 # A complete list of these tags are:
-GARBAGE_TAGS: list[str] = ["<SIL>", "<MUSIC>", "<NOISE>", "<OTHER>"]
+GS_GARBAGE_TAGS: list[str] = ["<SIL>", "<MUSIC>", "<NOISE>", "<OTHER>"]
