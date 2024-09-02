d28cc91bf24a602d4f06b2da62ce7d4aad8178f5
anilkeshwani <anilkeshwani@hotmail.com>
2024-09-02 23:02:30 +0000

    Implement PUA DSUs for interleaving

--- scripts/interleave.py
+++ scripts/interleave.py
@@ -5,7 +5,7 @@ import logging
 import os
 import sys
 from argparse import ArgumentParser, Namespace
-from itertools import zip_longest
+from itertools import groupby, zip_longest
 from pathlib import Path
 
 import numpy as np
@@ -24,7 +24,7 @@ from sardalign.constants import (
     SPEECH_TOKENS_KEY,
     TOKENIZED_KEY,
 )
-from sardalign.utils import count_lines
+from sardalign.utils import count_lines, dsu2pua
 from sardalign.utils.align import times_to_hubert_idxs
 
 
@@ -51,6 +51,12 @@ def parse_args() -> Namespace:
         dest="use_modality_tokens",
         help="Do no prepend special modality tokens to spans of text/speech tokens",
     )
+    parser.add_argument(
+        "--no-deduplication",
+        action="store_false",
+        dest="deduplicate",
+        help="Do not deduplicate consecutive DSUs",
+    )
     parser.add_argument("--seed", type=int, default=SEED, help="Random seed")
     args = parser.parse_args()
     if args.output_jsonl is None:
@@ -64,15 +70,17 @@ def get_span_idxs_binomial(n: int, p: float, seq_len: int, seed: int = SEED) -> 
     return [0] + subspan_idxs[subspan_idxs < seq_len].tolist() + [seq_len]
 
 
-def interleave_dataset(input_jsonl: Path, output_jsonl: Path, use_modality_tokens: bool, seed: int = SEED) -> Path:
+def interleave_dataset(
+    input_jsonl: Path, output_jsonl: Path, use_modality_tokens: bool, deduplicate: bool, seed: int = SEED
+) -> Path:
     with open(input_jsonl, mode="r") as f, open(output_jsonl, mode="x") as g:
-        for i, line in enumerate(tqdm(f, desc="Interleaving text-speech samples")):
+        for i, line in enumerate(tqdm(f, desc="Interleaving text-speech samples", total=count_lines(input_jsonl))):
             sample = json.loads(line)
             start_with_text = i % 2 == 0  # even-indexed samples start with text tokens
             tokens = sample[TOKENIZED_KEY]
             align_t_starts = sample[ALIGNMENT_START_TIME_KEY]
             align_t_ends = sample[ALIGNMENT_END_TIME_KEY]
-            speech_tokens = sample[SPEECH_TOKENS_KEY]
+            speech_tokens: list[int] = sample[SPEECH_TOKENS_KEY]
             span_idxs = get_span_idxs_binomial(int(MEAN_MLS_SEQ_LEN), BINOM_PROB, len(tokens), seed)
             # idxs: list of 2-tuples of start and end indices of subspans e.g. [(0, 4), (11, 16), (21, 25), (28, 31)]
             idxs1, idxs2 = zip(span_idxs[:-1:2], span_idxs[1::2]), zip(span_idxs[1:-1:2], span_idxs[2::2])
@@ -86,9 +94,9 @@ def interleave_dataset(input_jsonl: Path, output_jsonl: Path, use_modality_token
                     HUBERT_DOWNSAMPLING_RATIO,
                 )
                 sp_tkns_spn = speech_tokens[start_idx_hu:end_idx_hu]
-                # TODO add functionality for optional de-duplication of HuBERT speech tokens
-                # TODO refactor for use with PUA chars
-                hubert_spans.append("".join([HUBERT_TOKEN_FSTRING.format(sp_tkn) for sp_tkn in sp_tkns_spn]))
+                if deduplicate:
+                    sp_tkns_spn = [k for k, g in groupby(sp_tkns_spn)]
+                hubert_spans.append("".join([dsu2pua(sp_tkn) for sp_tkn in sp_tkns_spn]))
 
             if use_modality_tokens:
                 text_spans = [" ".join((MODALITY_TOKEN_TEXT, text_span)) for text_span in text_spans]
