268ea4c78625333d68b6eedbf7b4d6099fba19b7
anilkeshwani <anilkeshwani@hotmail.com>
2024-09-02 14:04:14 +0000

    Update TINYLLAMA_* files constants

--- sardalign/constants/tinyllama.py
+++ sardalign/constants/tinyllama.py
@@ -1,14 +1,18 @@
 TINYLLAMA_HF_REPO: str = "TinyLlama/TinyLlama-1.1B-intermediate-step-1431k-3T"
 TINYLLAMA_HF_REVISION: str = "036fa4651240b9a1487f709833b9e4b96b4c1574"
-TINYLLAMA_HF_FILES: tuple[str, ...] = (
-    ".gitattributes",
-    "README.md",
+TINYLLAMA_HF_TOKENIZER_FILES: tuple[str, ...] = (
+    "special_tokens_map.json",
+    "tokenizer_config.json",
+    "tokenizer.json",
+    "tokenizer.model",
+)
+TINYLLAMA_HF_MODEL_FILES: tuple[str, ...] = (
     "config.json",
     "generation_config.json",
     "model.safetensors",
+)
+TINYLLAMA_HF_AUXILIARY_FILES: tuple[str, ...] = (
+    ".gitattributes",
+    "README.md",
     "pytorch_model.bin",
-    "special_tokens_map.json",
-    "tokenizer.json",
-    "tokenizer.model",
-    "tokenizer_config.json",
 )
