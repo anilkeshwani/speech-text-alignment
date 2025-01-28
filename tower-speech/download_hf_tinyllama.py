42d3ba539c3e38ebf77128c6781afa8fd529e369
anilkeshwani <anilkeshwani@hotmail.com>
2025-01-28 16:28:59 +0100

    Remove legacy Tower Speech doc and minor scripts

--- tower-speech/download_hf_tinyllama.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-# Load model directly
-from transformers import AutoModelForCausalLM, AutoTokenizer
-
-
-tokenizer = AutoTokenizer.from_pretrained("TinyLlama/TinyLlama-1.1B-intermediate-step-1431k-3T")
-model = AutoModelForCausalLM.from_pretrained("TinyLlama/TinyLlama-1.1B-intermediate-step-1431k-3T")
