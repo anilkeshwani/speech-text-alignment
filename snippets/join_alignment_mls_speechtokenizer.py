561ef3a8ea844fd82e211876096a493d7b135da4
anilkeshwani <anilkeshwani@hotmail.com>
2025-06-06 15:26:59 +0000

    Simplify join_alignment_mls_speechtokenizer.py
    This commit also introduces or identifies a BUG that may be in HF. We cannot pass "dev" or "validation" with those two names mapped to the same split despite the Hugging Face documentation which says:
    
    There are several ways to name splits. Validation splits are sometimes called “dev”, and test splits may be referred to as “eval”. These other split names are also supported, and the following keywords are equivalent
    
    Source: https://huggingface.co/docs/datasets/en/repository_structure#split-name-keywords

--- snippets/join_alignment_mls_speechtokenizer.py
+++ snippets/join_alignment_mls_speechtokenizer.py
@@ -3,84 +3,55 @@
 """Joins the time alignment columns onto the MLS dataset encoded as SpeechTokenizer RVQ tokens."""
 
 import gc
-import logging
-import os
-import sys
-from argparse import ArgumentParser, Namespace
 from math import ceil
 from pathlib import Path
 
-from datasets import Dataset, DatasetDict, load_dataset, NamedSplit
-from huggingface_hub import HfApi
-from pandas import DataFrame
-
-from sardalign.config import LOG_DATEFMT, LOG_FORMAT, LOG_LEVEL
+from datasets import load_dataset, Split
 
 
-logging.basicConfig(
-    format=LOG_FORMAT,
-    datefmt=LOG_DATEFMT,
-    level=os.environ.get("LOGLEVEL", LOG_LEVEL).upper(),
-    stream=sys.stdout,
-)
-
-LOGGER = logging.getLogger(__file__)
-
-HF_DATASETS_BASE_URL: str = "https://huggingface.co/datasets"
-REPO_TYPE: str = "dataset"
+# Constants
+MLS_SIZES = {"train": 10_808_037, "dev": 3_807, "test": 3_769}
 
 # Arguments
 CHUNK_SIZE = 100_000
-SPLIT = "train"
-REPO_NAME: str = "mls-speechtokenizer-RVQ-0-aligned"
-PRIVATE: bool = False
-COMMIT_MESSAGE: str = "Upload dataset"
-LOCAL_PARQUET_OUTPUT_DIR: Path = Path("/mnt/scratch-artemis/anilkeshwani/mls-st-aligned")
+SPLIT = Split.VALIDATION  # "train", "dev", or "test" # TODO BUG HF rejects "dev" or "validation" resp.
+ALIGNED_REPO_ID: str = "anilkeshwani/MLS_english_train_strat_sample_aligned_hubert"
+MLS_STOK_REPO_ID: str = "anilkeshwani/mls-speechtokenizer"
+JOINED_REPO_ID: str = "mls-speechtokenizer-RVQ-0-aligned"
+LOCAL_PARQUET_OUTPUT_DIR: Path = Path("/mnt/scratch-artemis/anilkeshwani/mls-st-aligned/dev").resolve()
 
-hfapi = HfApi()
-wai = hfapi.whoami()
-username = wai["name"]
-LOGGER.info(f"Logged in as user {username} ({wai['fullname']}. Email: {wai['email']})")
-REPO_ID = "/".join((username, REPO_NAME))
+aligned = load_dataset(ALIGNED_REPO_ID, split="dev").remove_columns(["speech_tokens"]).to_pandas()
+stok = load_dataset(MLS_STOK_REPO_ID, split="validation").remove_columns([f"RVQ_{i}" for i in range(1, 8)]).to_pandas()
 
-mls_aligned = (
-    load_dataset("anilkeshwani/mls-hubert_large_ll60k-layer_22", split=SPLIT)
-    .remove_columns(["speech_tokens"])
-    .to_pandas()
-)
-mls_st = (
-    load_dataset("anilkeshwani/mls-speechtokenizer", split=SPLIT)
-    .remove_columns([f"RVQ_{i}" for i in range(1, 8)])
-    .to_pandas()
-)
+# Check datasets contain matching samples
+ids_match = (aligned["ID"] == aligned["ID"]).all()
+if not ids_match:
+    raise ValueError("IDs do not match along the series. This may indicate a problem with the dataset.")
+else:
+    print("IDs match")
 
-mls_st.rename(columns={"RVQ_0": "speech_tokens"}, inplace=True)
-LOGGER.info("Renamed column RVQ_0 -> speech_tokens")
+# Rename RVQ_0 to speech_tokens (standard key)
+stok.rename(columns={"RVQ_0": "speech_tokens"}, inplace=True)
+print("Renamed column RVQ_0 -> speech_tokens")
+joined = aligned.merge(stok, on="ID", how="inner")  # Inner join on a shared key "ID"
 
-LOGGER.info(f"{type(mls_aligned['ID']) = }")
-LOGGER.info(f"{type(mls_st['ID']) = }")
-LOGGER.info(f"{len(mls_aligned['ID']) = }")
-LOGGER.info(f"{len(mls_st['ID']) = }")
-
-LOGGER.info(f"Check IDs match along series: {(mls_aligned['ID'] == mls_aligned['ID']).all()}")
-
-# Inner join on a shared key "ID"
-joined: DataFrame = mls_aligned.merge(mls_st, on="ID", how="inner")
-
-del mls_aligned, mls_st
+# Free up memory
+del aligned, stok
 gc.collect()
 
-LOGGER.info(f"{len(joined) = :,}")
-LOGGER.info(f"{joined.columns = }")
+print(f"{len(joined) = :,}")
+print(f"{joined.columns = }")
 
-# Chunk and serialise as Parquet #############################################
+if not LOCAL_PARQUET_OUTPUT_DIR.exists():
+    print(f"Creating output directory: {LOCAL_PARQUET_OUTPUT_DIR}")
+    LOCAL_PARQUET_OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
 
+# Serialise as Parquet
 n_chunks = ceil(len(joined) / CHUNK_SIZE)
-LOGGER.info(f"Splitting dataset into {n_chunks} chunks of size {CHUNK_SIZE:,} each...")
-
+print(f"Splitting dataset into {n_chunks} chunks of size {CHUNK_SIZE:,} each...")
 for i in range(n_chunks):
-    chunk: DataFrame = joined.iloc[i * CHUNK_SIZE : min((i + 1) * CHUNK_SIZE, len(joined))]
-    LOGGER.info(f"Processing chunk {i + 1} of {n_chunks} (size: {len(chunk):,})")
+    chunk = joined.iloc[i * CHUNK_SIZE : min((i + 1) * CHUNK_SIZE, len(joined))]
+    print(f"Processing chunk {i + 1} of {n_chunks} (size: {len(chunk):,})")
     chunk_label = str(i + 1).zfill(len(str(n_chunks)))
     # Serialise as Parquet using pandas.DataFrame.to_parquet
-    chunk.to_parquet(LOCAL_PARQUET_OUTPUT_DIR / f"data-{chunk_label}-of-{n_chunks}-{REPO_NAME}-{SPLIT}.parquet")
+    chunk.to_parquet(LOCAL_PARQUET_OUTPUT_DIR / f"data-{chunk_label}-of-{n_chunks}-{JOINED_REPO_ID}-{SPLIT}.parquet")
