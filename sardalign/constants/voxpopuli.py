0a8bac1f21e2adf261cc53ff0b682d875c48a67f
anilkeshwani <anilkeshwani@hotmail.com>
2025-09-26 14:51:41 +0000

    Generalise scripts/voxpopuli/preprocess_hf_voxpopuli.py to allow loading VoxPopuli from disk
    Added a Sardine servers specific constant for the VoxPopuli path in sardalign/constants/voxpopuli.py

--- sardalign/constants/voxpopuli.py
+++ sardalign/constants/voxpopuli.py
@@ -1,1 +1,5 @@
 VOXPOPULI_HF_DATASET_REPO: str = "facebook/voxpopuli"
+# NOTE VOXPOPULI_LOCAL_DISK_DIR specific to our mnt fs
+# NOTE must concat dataset subset to path e.g. Path(VOXPOPULI_LOCAL_DISK_DIR) / "en"
+# subsets comprise: cs  de  en  es  et  fi  fr  hr  hu  it  lt  nl  pl  ro  sk  sl
+VOXPOPULI_LOCAL_DISK_DIR: str = "/mnt/scratch-artemis/shared/datasets/facebook/voxpopuli/"
